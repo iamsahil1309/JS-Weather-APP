6547411ebf2a55a4ae8440f79875e1fdd71ce680
Sahil Singh <kswayam2000@gmail.com>
2026-08-16 12:31:03 +0530

    setup api and finish project

--- app.js
+++ app.js
@@ -1,30 +1,52 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const inputCity = document.getElementsByClassName('city-input')
-    const getWeather = document.getElementsByClassName('get-weather')
-    const error = document.getElementsByClassName('error-message')
-    const weatherDetail = document.getElementsByClassName('weather-detail')
-    const cityName = document.getElementsByClassName('city-name')
-    const tempratureDisplay = document.getElementsByClassName('temprature')
-    const descriptionDisplay = document.getElementsByClassName('description')
+    const cityInput = document.getElementById("city-input");
+    const getWeatherBtn = document.getElementById("get-weather-btn");
+    const weatherInfo = document.getElementById("weather-info");
+    const cityNameDisplay = document.getElementById("city-name");
+    const temperatureDisplay = document.getElementById("temperature");
+    const descriptionDisplay = document.getElementById("description");
+    const errorMessage = document.getElementById("error-message");
 
-    getWeather.addEventListener('click',async () => {
-        const city = inputCity.value.trim()
+    weather_api = "bc95536ffe94b5fc5a73f86df61c4ac2";
+
+    getWeatherBtn.addEventListener('click', async() => {
+        const city = cityInput.value.trim()
         if(!city) return
         try {
             const weatherData = await fetchWeatherData(city)
             displayWeatherData(weatherData)
+            cityInput.value=""
         } catch (error) {
-            errorMsg()
+            cityInput.value = "";
+            showError()
         }
     })
 
-    async function fetchWeatherData(city){}
-
-    function displayWeatherData(){}
-
-    function errorMsg(){
-        weatherDetail.classList.add('hidden')
-        error.classList.remove('hidden')
+    async function fetchWeatherData(city) {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${weather_api}`
+        const response = await fetch(url)
+        if(!response.ok) {
+            throw new Error('City not found. Try Again!')
+        }
+        const data = response.json()
+        return data
     }
+
+    function displayWeatherData(data) {
+        weatherInfo.classList.remove('hidden')
+        errorMessage.classList.add('hidden')
+
+        const {name, main, weather} = data
+        cityNameDisplay.textContent = `${name}`
+        temperatureDisplay.textContent = `Temprature : ${main.temp}`
+        descriptionDisplay.textContent = `Description : ${weather[0].description}`
+
+    }
+
+    function showError(){
+        weatherInfo.classList.add('hidden')
+        errorMessage.classList.remove('hidden')
+    }
+
 
 })
